fcd3cf73b646a1000c74ecf11e75aab98069fe25
Duc Minh Vu <duc.minh.vu@campus.tu-berlin.de>
2022-09-08 17:34:32 +0700

    all done

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class Board {
-    private int n;
-    private char [][] theboard;
-    private List<int[]> moves;
+    private final int n;
+    private final char [][] theboard;
+    private final List<int[]> moves;
 
     // Constructor
     public Board (int n) {
@@ -21,6 +21,9 @@ public class Board {
 
     public List<int[]> getMoves() {
         return moves;
+    }
+
+    public void setTheboard(Board boardd) {
     }
 
     public char[][] getTheboard() {
@@ -49,37 +52,10 @@ public class Board {
         System.out.println("\n");
     }
 
-    public void fillintable () {
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                char temp ;
-                String asker = String.format("Item at position at column: %d, row: %d", i, j);
 
-                System.out.println(asker);
-                Scanner sc = new Scanner(System.in);
-                temp = sc.next().charAt(0);
-                if (temp == 'x') {
-                    int[] coor = new int[2];
-                    coor[0] = i;
-                    coor[1] = j;
-                    moves.add(coor);
-                }
-                while (temp != '1'&& temp != '0' && temp != 'x') {
-                    System.out.println("invalid input");
-                    temp = sc.next().charAt(0);
-                }
-                filltoken(i, j, temp);
-                System.out.println(Arrays.deepToString(theboard).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
-            }
-
-        }
-    }
 
     public void fillintokensplit () {
-        char[][] sampleboard = new char[4][4];
         String input;
-
-
 
         // Displaying Msg
         System.out.println("Insert a board here:");
@@ -93,9 +69,7 @@ public class Board {
             // if nums of lines doesnt match n
             if (lines.length != n) {
                 throw  new InputMismatchException("Invalid Board");
-//                System.out.println("Invalid Board");
-//                System.out.println("Please try again");
-//                input = sc.nextLine();
+
             }
             for (int j = 0; j < n; j++) {
                 char[] tokens = lines[j].toCharArray();
@@ -117,19 +91,4 @@ public class Board {
         System.out.println(Arrays.deepToString(theboard).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
     }
 
-
-    public void printbeginning () {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Board-size");
-       n = sc.nextInt();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Enter Board-size");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Board board = new Board(n);
-        board.printemptyboard();
-        board.fillintokensplit();
-    }
 }
--- src/Logik.java
+++ src/Logik.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Logic {
-    private Board board;
+public class Logik {
+    private final Board board;
 
 
-    public Logic (Board board) {
+    public Logik (Board board) {
         this.board= board;
 
     }
@@ -19,47 +19,49 @@ public class Logic {
     }
 
     // check if there are 3 0's or 1's found on the board
-    public void checkrule3 () {
+    public boolean checkrule3 (Board boardd) {
         if (board.getN() >= 3) {
             for (int i = 0; i < board.getN() ; i++) {
                 for (int j = 0; j < board.getN() - 2; j++) {
-                    if (board.getTheboard()[i][j] == board.getTheboard()[i][j+1] &&
-                            board.getTheboard()[i][j+1] == board.getTheboard()[i][j+2]
-                    && board.getTheboard()[i][j] != 'x') {
-                        throw new InputMismatchException("3 consecutive numbers found");
+                    if (boardd.getTheboard()[i][j] == boardd.getTheboard()[i][j+1] &&
+                            boardd.getTheboard()[i][j+1] == boardd.getTheboard()[i][j+2]
+                            && boardd.getTheboard()[i][j] != 'x') {
+                        return false;
                     }
                 }
             }
 
             for (int i = 0; i < board.getN() -2; i++) {
                 for (int j = 0; j < board.getN(); j++) {
-                    if (board.getTheboard()[i][j] == board.getTheboard()[i+1][j] &&
-                        board.getTheboard()[i+1][j] == board.getTheboard()[i+2][j]
-                        && board.getTheboard()[i][j] != 'x') {
-                        throw new InputMismatchException("3 consecutive numbers found");
+                    if (boardd.getTheboard()[i][j] == boardd.getTheboard()[i+1][j] &&
+                            boardd.getTheboard()[i+1][j] == boardd.getTheboard()[i+2][j]
+                            && boardd.getTheboard()[i][j] != 'x') {
+                        return false;
 
                     }
                 }
             }
         }
+        return true;
     }
     // check if there are the same number of 1's and 0's on the board
-    public void checkrule1 () {
+    public boolean checkrule1 (Board boardd) {
         for (int i = 0; i < board.getN(); i++) {
             int count0 = 0;
             int count1 = 0;
 
             for (int j = 0; j < board.getN(); j++) {
-                if (board.getTheboard()[i][j] == '0') {
+                if (boardd.getTheboard()[i][j] == '0') {
                     count0++;
                 }
 
-                if (board.getTheboard()[i][j] == '1') {
+                if (boardd.getTheboard()[i][j] == '1') {
                     count1++;
                 }
             }
             if (count0 > board.getN()/2 || count1 > board.getN()/2) {
-                throw new InputMismatchException("Numbers of 0's and 1's on the same row are not the same");
+//                throw new InputMismatchException("Numbers of 0's and 1's on the same row are not the same");
+                return false;
             }
         }
 
@@ -68,34 +70,34 @@ public class Logic {
             int count1 = 0;
 
             for (int j = 0; j < board.getN(); j++) {
-                if (board.getTheboard()[j][i] == '0') {
+                if (boardd.getTheboard()[j][i] == '0') {
                     count0++;
                 }
 
-                if (board.getTheboard()[j][i] == '1') {
+                if (boardd.getTheboard()[j][i] == '1') {
                     count1++;
                 }
             }
             if (count0 > board.getN()/2 || count1 > board.getN()/2) {
-                throw new InputMismatchException("Numbers of 0's and 1's on the same row are not the same");
+//                throw new InputMismatchException("Numbers of 0's and 1's on the same row are not the same");
+                return false;
             }
         }
-
+        return true;
     }
 
     // Check for duplicate Rows or Column
-    public void checkrule2 () {
+    public boolean checkrule2 (Board boardd) {
         Map<String, Integer> checker = new HashMap<>();
 
-        for (char[] x: board.getTheboard()) {
+        for (char[] x: boardd.getTheboard()) {
             String convert = Arrays.toString(x);
             if(!checker.containsKey(convert) || convert.contains("x")) {
                 boolean a = checker.containsKey(convert);
                 checker.put(convert, 1);
             }
             else {
-                String str = "At least 2 duplicate rows found";
-                throw new InputMismatchException(str);
+                return false;
             }
         }
 
@@ -103,16 +105,16 @@ public class Logic {
         Map<String, Integer> checker2 = new HashMap<>();
 
         for (int i = 0; i < board.getN(); i++) {
-            char[] tempcoloumn = getColumn(board.getTheboard(), i);
+            char[] tempcoloumn = getColumn(boardd.getTheboard(), i);
             String convert = Arrays.toString(tempcoloumn);
             if (!checker2.containsKey(convert) || convert.contains("x")) {
                 checker2.put(convert, 1);
             }
             else {
-                String str = "At least 2 duplicate column found";
-                throw new InputMismatchException(str);
+                return false;
             }
         }
+        return true;
     }
 
     public void check2op () {
@@ -127,29 +129,7 @@ public class Logic {
             throw new InputMismatchException(str);
         }
     }
-    public void checkall() {
-        checkrule1();
-        checkrule2();
-//        check2op();
-        checkrule3();
+    public boolean checkall(Board boardd) {
+        return checkrule1(boardd) && checkrule2(boardd) && checkrule3(boardd);
     }
-
-    public static void main(String[] args) {
-        System.out.println("Enter Board-size");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Board board = new Board(n);
-
-        board.printemptyboard();
-//        board.fillintable();
-        board.fillintokensplit();
-
-        Logic logic = new Logic(board);
-        System.out.println("Checking if board is valid...");
-        logic.checkall();
-        System.out.println("Board is valid");
-        System.out.println(board.getMoves());
-    }
-
-
 }
